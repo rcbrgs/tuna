3c70ffc465d60709b9ffe5a76766d283eafbbd2f
Renato Callado Borges <rborges@hush.com>
2014-11-19 15:50:12 +0100

    Now 0MQ stub is contained to logging module, which yes, is now a local Python module.

--- tuna_logging/tuna_logging.py
+++ tuna_logging/tuna_logging.py
@@ -4,25 +4,43 @@
 Logging module for Tuna, using ZeroMQ for message passing.
 """
 
-import logging
+# 0MQ stub
+
 import zmq
-
-logging.basicConfig ( filename = '/home/nix/tuna.log', level = logging.DEBUG )
-
-logging.info ( "Tuna logging module started." )
-logging.debug ( "Logging threshold: logging DEBUG or higher." )
-
 zmq_context = zmq.Context ( )
 
+# definitions
+
 def main ( ):
+    # config logging module
+    import logging
+    logging.basicConfig ( filename = '/home/nix/tuna.log', level = logging.DEBUG )
+    logging.info ( "Tuna logging module started." )
+    logging.debug ( "Logging threshold: logging DEBUG or higher." )
     # instantiate a REP node 
     zmq_socket = zmq_context.socket ( zmq.REP )
     zmq_socket.bind ( "tcp://*:5000" )
     # log incoming messages
     while True:
         msg = zmq_socket.recv ( )
-        logging.debug ( "%s", msg )
+        logging.debug ( "%s" % msg.decode("utf-8") )
         zmq_socket.send ( b'ACK' )
+
+class tuna_log_client:
+    def __init__ ( self ):
+        self.zmq_context = zmq.Context ( )
+        self.zmq_socket = self.zmq_context.socket ( zmq.REQ )
+        self.zmq_socket.connect ( "tcp://127.0.0.1:5000" )
+
+    def log ( self, msg ):
+        self.zmq_socket.send ( msg )
+        answer = self.zmq_socket.recv ( )
+        if answer.decode("utf-8") != 'ACK':
+            print ( u'Something is fishy!' )
+            print ( u'Received: "%s".' % answer.decode("utf-8") )
+            print ( u"Expected: 'ACK'" )
+
+# control
 
 if __name__ == "__main__":
     main ( )
