68f5d26869aace2044f31e591fb22466249cc355
Renato Callado Borges <rborges@hush.com>
2014-11-21 13:14:28 +0100

    Opens a window, attached to the log.

---
+++ tuna_viewer_2d/.#tuna_viewer_2d.py
@@ -0,0 +1,1 @@
+nix@einstein.oamp.lan.6920:1416557460
--- tuna_viewer_2d/tuna_viewer_2d.py
+++ tuna_viewer_2d/tuna_viewer_2d.py
@@ -6,13 +6,22 @@ Basic viewer widget for 2d images. As standalone, minimal GUI for opening FITS f
 
 import sys
 sys.path.append ( '/home/nix/cloud_essential2/temp/btfi/tuna2/tuna_logging' )
-
 import tuna_logging
+
+#from PyQt4 import QtGui
+import PyQt4.QtGui
 
 def main ( ):
     tuna_log = tuna_logging.tuna_log_client ( )
     log = tuna_log.log
-    #log ( b'test' )
+
+    app = PyQt4.QtGui.QApplication ( sys.argv )
+    w = PyQt4.QtGui.QWidget ( )
+    w.resize ( 250, 150 )
+    w.move ( 300, 300 )
+    w.setWindowTitle ( 'Simple' )
+    w.show ( )
+    sys.exit ( app.exec_ ( ) )
 
 if __name__ == "__main__":
     main ( )
