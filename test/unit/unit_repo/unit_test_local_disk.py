046684dfc20b2320697d3e819568e82e1234c2ed
Renato Callado Borges <rborges@hush.com>
2014-11-28 16:37:01 +0100

    Added stub unit test code to stub unit tests.

---
+++ test/unit/unit_repo/unit_test_local_disk.py
@@ -0,0 +1,14 @@
+import unittest
+
+class unit_test_tuna_log_server ( unittest.TestCase ):
+    def setUp ( self ):
+        pass
+
+    def test_ ( self ):
+        pass
+
+    def tearDown ( self ):
+        pass
+
+if __name__ == '__main__':
+    unittest.main ( )
