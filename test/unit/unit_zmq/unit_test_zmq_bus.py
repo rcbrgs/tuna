6735c77919caf1da5eb2e581dfe2bcc87d939de0
Renato Callado Borges <rborges@hush.com>
2014-12-08 17:21:49 +0100

    zmq_bus first unit test added. Refactored zmq_bus to shutdown gracefully and to have "targets" for the messages it receives. zmq_bus unit test requires the bus to run in a separate thread. In the init of this threaded object could not find the syntax for super(), using hardcoded call instead. Enabling second test of this unit fails, prolly needs setUp refactor.

--- test/unit/unit_zmq/unit_test_zmq_bus.py
+++ test/unit/unit_zmq/unit_test_zmq_bus.py
@@ -1,14 +1,41 @@
 import unittest
 
-class unit_test_tuna_log_server ( unittest.TestCase ):
+class unit_test_zmq_bus ( unittest.TestCase ):
     def setUp ( self ):
-        pass
+        import threading
+        class threaded_bus ( threading.Thread ):
+            def __init__ ( self ):
+                #super ( threaded_bus, self ).__init ( )
+                threading.Thread.__init__ ( self )
+                import sys
+                sys.path.append ( '/home/nix/sync/tuna' )
+                from github.zmq import zmq_bus
+                self.zmq_bus_instance = zmq_bus.zmq_bus ( )
+            def close ( self ):
+                self.zmq_bus_instance.close ( )
+            def run ( self ):
+                self.zmq_bus_instance.run ( )
+        self.threaded_bus_instance = threaded_bus ( )
+        self.threaded_bus_instance.start ( )
+        import zmq
+        self.zmq_context = zmq.Context ( )
+        self.zmq_socket_req = self.zmq_context.socket ( zmq.REQ )
+        self.zmq_socket_req.connect ( "tcp://127.0.0.1:5000" )
 
-    def test_ ( self ):
-        pass
+    def test_zmq_bus_replies ( self ):
+        self.zmq_socket_req.send ( b'test: replies ok' )
+        answer = self.zmq_socket_req.recv ( )
+        self.assertEqual ( answer, b'ACK' )
+
+#    def test_zmq_bus_requests ( self ):
+#        pass
+
+#    def test_zmq_bus_forwards ( self ):
+#        pass
 
     def tearDown ( self ):
-        pass
+        self.threaded_bus_instance.close ( )
+        self.threaded_bus_instance.join ( )
 
 if __name__ == '__main__':
     unittest.main ( )
--- zmq/zmq_bus.py
+++ zmq/zmq_bus.py
@@ -20,28 +20,65 @@ class zmq_bus ( ):
         self.__zmq_socket_req = self.__zmq_context.socket ( zmq.REQ )
         self.__zmq_socket_rep = self.__zmq_context.socket ( zmq.REP )
         self.__zmq_socket_rep.bind ( "tcp://127.0.0.1:5000" )
+        self.__lock = True
 
+    def __call_log ( self, msg ):
+        """
+        Dispatch msg to log server.
+        """
+
+        self.__zmq_socket_req.connect ( "tcp://127.0.0.1:5001" )
+        self.__zmq_socket_req.send ( msg.encode("utf-8") )
+        answer = self.__zmq_socket_req.recv ( ) 
+        if answer.decode ( "utf-8" ) != 'ACK':
+            print ( u'Something is fishy!' )
+            print ( u'Received: "%s" from %s.' % answer.decode("utf-8"), msg_destination )
+            print ( u"Expected: 'ACK'" )
+
+    def __call_test ( self, msg ):
+        """
+        Test procedure for zmq_bus.
+        """
+        print ( "zmq_bus received msg: %s." % msg )
+
+
+    def check_ACK ( self, ack_msg ):
+        if ack_msg.decode ( "utf-8" ) != 'ACK':
+            print ( u'Something is fishy!' )
+            print ( u'Received: "%s" from %s.' % answer.decode("utf-8"), msg_destination )
+            print ( u"Expected: 'ACK'" )
+
+    def close ( self ):
+        """
+        Gracefully shutdown this process.
+        """
+        
+        self.__lock = False
+        self.__zmq_socket_req.connect ( "tcp://127.0.0.1:5000" )
+        self.__zmq_socket_req.send ( b'test: Shutting down tuna zmq bus.' )
+        answer = self.__zmq_socket_req.recv ( )
+        self.check_ACK ( answer )
 
     def run ( self ):
         """
         Orchestrate incoming messages.
 
         This method will run in loop, listening to messages and dispatching them as appropriated.
+
+        destination_call_table is a dictionary associating target strings with the functions to be run. The services responsible for a given target can be changed here without changing the clients.
         """
 
-        while True:
+        destination_call_table = {
+            'log'  : self.__call_log,
+            'test' : self.__call_test }
+
+        while self.__lock == True:
             msg = self.__zmq_socket_rep.recv ( )
             msg_partition = str ( msg, ( "utf-8" ) ).partition ( ": " )
             msg_destination = msg_partition[0]
-            if msg_destination == 'log':
-                self.__zmq_socket_req.connect ( "tcp://127.0.0.1:5001" )
-                self.__zmq_socket_req.send ( msg_partition[2].encode("utf-8") )
-                answer = self.__zmq_socket_req.recv ( ) 
-                if answer.decode ( "utf-8" ) != 'ACK':
-                    print ( u'Something is fishy!' )
-                    print ( u'Received: "%s" from %s.' % answer.decode("utf-8"), msg_destination )
-                    print ( u"Expected: 'ACK'" )
-                self.__zmq_socket_rep.send ( b'ACK' )
+            msg_contents = msg_partition[2]
+            destination_call_table [ msg_destination ] ( msg_contents )
+            self.__zmq_socket_rep.send ( b'ACK' )
 
 def main ( ):
     zmq_proxy = zmq_bus ( )
