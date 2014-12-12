2167498261a338eb8ccfed03298a52ae18161a71
Renato Callado Borges <rborges@hush.com>
2014-12-12 17:19:15 +0100

    Changed from a central widget to a QDockWidget.

---
+++ gui/widget_viewer_2d.py
@@ -0,0 +1,17 @@
+"""
+widget_viewer_2d displays 2d images in a dock.
+
+It is composed of a QDockWidget that displays a QPixMap. Eventually, more image-related functions will be added here.
+"""
+
+import PyQt4.QtGui
+
+class widget_viewer_2d ( PyQt4.QtGui.QDockWidget ):
+    def __init__ ( self, image_pixmap=PyQt4.QtGui.QPixmap, *args, **kwargs ):
+        self.image_canvas = image_pixmap
+        super ( widget_viewer_2d, self ).__init__ ( *args, **kwargs )
+        self.canvas_label = PyQt4.QtGui.QLabel ( self )
+        self.canvas_label.setPixmap ( self.image_canvas )
+        self.setWidget ( self.canvas_label )
+        #self.setFloating ( False )
+        self.show ( )
--- test/unit/unit_gui/unit_test_simple_gui.py
+++ test/unit/unit_gui/unit_test_simple_gui.py
@@ -1,10 +1,18 @@
 import unittest
+#import sys
+#sys.path.append ( '/home/nix/sync/tuna/github' )
 
-class unit_test_tuna_log_server ( unittest.TestCase ):
+class unit_test_tuna_simple_gui ( unittest.TestCase ):
     def setUp ( self ):
         pass
 
-    def test_ ( self ):
+    def test_dock_widget ( self ):
+        # dock can be created
+        mock_simple_gui = unittest.mock.MagicMock ( )
+        # dock can be hidden
+        # dock can be moved
+        # dock can be restored
+        # dock can be destroyed
         pass
 
     def tearDown ( self ):
