b0366114c28f35f069dca6040fef4ea4e8dc25af
Renato Callado Borges <rborges@hush.com>
2015-01-08 16:43:45 +0100

    Polished the minimalist example.

--- cli_example1.sh
+++ cli_example1.sh
@@ -1,6 +1,12 @@
 #!/bin/env python3
 
+# Minimal source to use Tuna as a library:
 import tuna
-tuna_daemons = tuna.backend ( )
-# do tuna-related stuff here
+tuna_daemons = tuna.console.backend ( )
+tuna_daemons.start ( )
+
+# Write your code here:
+
+
+# Do not edit below this.
 tuna_daemons.close ( )
