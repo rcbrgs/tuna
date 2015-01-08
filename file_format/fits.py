c1d2d71fc1f59dde7d4d33bed560ee858e2dad79
Renato Callado Borges <rborges@hush.com>
2015-01-08 15:42:54 +0100

    Added feature to save fits files.

--- file_format/fits.py
+++ file_format/fits.py
@@ -1,6 +1,6 @@
 import numpy
 from .file_reader import file_reader
-import astropy.io.fits
+import astropy.io.fits as astrofits
 import warnings
 
 class fits ( file_reader ):
@@ -10,13 +10,15 @@ class fits ( file_reader ):
     Consists mostly of a wrapper around Astropy's io.fits module.
     """
 
-    def __init__ ( self, file_name = None, log = None ):
-        self._file_name = file_name
+    def __init__ ( self, file_name = None, log = None, image_ndarray = None ):
+        super ( fits, self ).__init__ ( )
         if log:
             self.log = log
         else:
             self.log = print
-        super ( fits, self ).__init__ ( )
+
+        self._file_name = file_name
+        self._image_ndarray = image_ndarray
 
     def get_image_ndarray ( self ):
         return self._image_ndarray
@@ -30,7 +32,7 @@ class fits ( file_reader ):
         try:
             with warnings.catch_warnings ( ):
                 warnings.simplefilter ( "ignore" )
-                hdu_list = astropy.io.fits.open ( self._file_name )
+                hdu_list = astrofits.open ( self._file_name )
             self.log ( "File %s opened as a FITS file." % self._file_name )
             self._image_ndarray = hdu_list[0].data
             self.log ( "Assigned data section of first HDU as the image ndarray." )
@@ -38,3 +40,14 @@ class fits ( file_reader ):
         except OSError as e:
             self.log ( "OSError: %s." % e )
             self._is_readable = False
+
+    def write ( self, file_name = None ):
+        if file_name != None:
+            self.__file_name = file_name
+        if self.__file_name == None:
+            self.log ( "No file_name for FITS write, aborting." )
+            return
+        
+        hdu = astrofits.PrimaryHDU ( self._image_ndarray )
+        hdu_list = astrofits.HDUList ( [hdu] )
+        hdu_list.writeto ( self.__file_name )
