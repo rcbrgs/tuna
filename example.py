bca995d11e0d826186e17c14f01327ed70e73544
Renato Callado Borges <rborges@hush.com>
2015-02-06 13:49:11 +0100

    Begun to create ROI examples.

--- example.py
+++ example.py
@@ -7,17 +7,23 @@ import tuna
 tuna_daemons = tuna.console.backend ( ) 
 tuna_daemons.start ( )
 
-def g093_metadata ( ):
+def g092_create_ROI_examples ( ):
 
-    g093 = tuna.io.read ( file_name = '/home/nix/cloud_fpdata1/2014-11-05_Benoit_ngc772/G093/G093.ADT' )
-    tuna.io.write ( file_name   = 'g093_fits_file.fits',
-	            array       = g093.get_array    ( ), 
-		    metadata    = g093.get_metadata ( ),
+    g092 = tuna.io.read ( file_name = '/home/nix/cloud_fpdata1/2014-11-05_Benoit_ngc772/G092/G092.AD3' )
+    g092_array = g092.get_array ( )
+    import numpy
+    central_region_ROI_array = numpy.ndarray ( shape = ( g092_array.shape[0], 100, 100 ) )
+    for row in range ( 100 ):
+        for col in range ( 100 ):
+            for dep in range ( g092_array.shape[0] ):
+                central_region_ROI_array[dep][row][col] = g092_array[dep][row + 170][col + 200]
+
+    tuna.io.write ( file_name   = 'g092_central_region_ROI.fits',
+	            array       = central_region_ROI_array, 
+		    metadata    = [],
 		    file_format = 'fits' )
-    g093 = tuna.io.read ( file_name = ( 'g093_fits_file.fits' ) )
-    print ( g093.get_metadata_parameter ( parameter = 'whitelevel' ) )
 
-g093_metadata ( )
+g092_create_ROI_examples ( )
 
 # This call is required to close the daemons gracefully:
 tuna_daemons.finish ( )
