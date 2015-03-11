01239185ffedc4d22a75b5bd71c5b3463564e3b3
Renato Callado Borges <rborges@hush.com>
2015-03-11 17:56:26 +0100

    Refactored to make clearer, trying to manually understand Airy.

--- tools/models/airy.py
+++ tools/models/airy.py
@@ -17,12 +17,12 @@ class airy ( object ):
         self.__a_filtered = numpy.copy ( a_filtered )
 
     def fit ( self,
-              I0 = 1., 
-              lamb = 0.6563, 
-              F = 15, 
-              n = 1, 
-              fcam = 0.1, 
-              pix = 9. ):
+              f_beam         = 1., 
+              f_wavelength   = 0.6563, 
+              f_finesse      = 15, 
+              f_reflectivity = 0.99, 
+              f_focal_length = 0.1, 
+              f_pixel_size   = 9. ):
         """
         Airy function in order to compute a ring
         Originally written by Benoît Epinat.
@@ -30,12 +30,12 @@ class airy ( object ):
         Parameters
         ----------
         
-        I0   = 1.     : intensity
-        lamb = 0.6563 : wavelength (micron)
-        F    = 15     : finesse
-        n    = 1      : reflexion index
-        fcam = 0.1    : focal length of the camera (meter)
-        pix  = 9.     : pixel size (micron)
+        f_beam         = 1.     : intensity
+        f_wavelength   = 0.6563 : wavelength (micron)
+        f_finesse      = 15     : finesse
+        f_reflectivity = 1      : reflexion index
+        f_focal_length = 0.1    : focal length of the camera (meter)
+        f_pixel_size   = 9.     : pixel size (micron)
         """
 
         i_deps = self.__a_filtered.shape [ 0 ]
@@ -46,30 +46,37 @@ class airy ( object ):
         # task: find the f_min_distance that has the innermost ring with the same distance as the raw data
         f_min_distance = 1904
         # task: find the f_max_distance
-        f_max_distance = 1926
+        #f_max_distance = 1926
+        f_max_distance = 1905
 
         #a_fit = numpy.zeros ( shape = ( 22, self.__a_filtered.shape [ 1 ], self.__a_filtered.shape [ 2 ] ) )
         a_fit = numpy.zeros ( shape = self.__a_filtered.shape )
 
+        l_spacings = [ ]
         #for i_dep in range ( a_fit.shape [ 0 ] ):
         for i_dep in range ( i_deps ):
-            # spacing between the two FP plates (micron)
-            f_spacing = f_min_distance + ( float ( i_dep ) / float ( i_deps - 1 ) ) * ( f_max_distance - f_min_distance )
-            image = numpy.zeros ( shape = ( i_rows, i_cols ) )
+            #a_image = numpy.zeros ( shape = ( i_rows, i_cols ) )
             # we need the indices of the pixels in image
-            ind = numpy.indices ( ( i_rows, i_cols ) )
+            a_indices = numpy.indices ( ( i_rows, i_cols ) )
             # we use numpy sqrt function because we compute on arrays
-            rpix = numpy.sqrt ( ( ind [ 0 ] - self.__t_center [ 0 ] ) ** 2 +
-                                ( ind [ 1 ] - self.__t_center [ 1 ] ) ** 2 )
+            a_distances = numpy.sqrt ( ( a_indices [ 0 ] - self.__t_center [ 0 ] ) ** 2 +
+                                       ( a_indices [ 1 ] - self.__t_center [ 1 ] ) ** 2 )
             # radius unit = meter
-            r = rpix * pix * ( 1.e-6 )
-            theta = numpy.arctan ( r / fcam )
+            a_grid = a_distances * f_pixel_size * ( 1.e-6 )
+            a_theta = numpy.arctan ( a_grid / f_focal_length )
+
+            # spacing between the two FP plates (micron)
+            #f_spacing = float ( f_min_distance + 
+            #                    float ( i_dep / ( i_deps - 1 ) ) * ( f_max_distance - f_min_distance ) )
+            f_spacing = float ( f_min_distance + ( i_dep / ( i_deps - 1 ) ) * ( f_max_distance - f_min_distance ) )
+            l_spacings.append ( f_spacing )
             # function of Airy I
-            f = 4.0 * ( ( F ** 2 ) ) / ( numpy.pi ** 2 ) 
-            phi = 2.0 * numpy.pi * ( ( 2 * n * f_spacing * ( numpy.cos ( theta ) ) ) / ( lamb ) )
-            I = I0 / ( 1. + f * ( numpy.sin ( phi / 2.0 ) ) ** 2 )
+            f_airy_I = 4.0 * ( ( f_finesse ** 2 ) ) / ( numpy.pi ** 2 ) 
+            f_phase = 4.0 * numpy.pi * f_reflectivity * f_spacing * numpy.cos ( a_theta ) / f_wavelength 
+            f_intensity = f_beam / ( 1. + f_airy_I * ( numpy.sin ( f_phase / 2.0 ) ) ** 2 )
             # append to cube
-            a_fit [ i_dep ] = I
+            a_fit [ i_dep ] = f_intensity
+        self.log ( "debug: l_spacings = %s" % str ( l_spacings ) )
 
         self.__a_fit = a_fit
 
