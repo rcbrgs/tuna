bc5527f126bd813abe35489e9f70cd4f132245e0
Renato Callado Borges <rborges@hush.com>
2015-04-08 13:39:55 +0200

    Added fixed parameters, geometry seems correct, but values are still far away.

--- tools/models/airy.py
+++ tools/models/airy.py
@@ -22,21 +22,25 @@ class airy_model ( Parametric2DModel ):
     """
 
     beam = Parameter ( 'beam' )
-    gap = Parameter ( 'gap' )
     finesse = Parameter ( 'finesse' )
+    focal_length = Parameter ( 'focal_length' )
+    gap = Parameter ( 'gap' )
     
     def __init__ ( self,
 #                   log = print,
                    beam = 1.,
-                   gap = 0.,
                    finesse = 15,
+                   focal_length = 0.1,
+                   gap = 0.,
+
                    center = ( int, int ), 
                    discontinuum = numpy.ndarray,
                    **constraints ):
 
         super ( airy_model, self ).__init__ ( beam = beam,
-                                              gap = gap,
                                               finesse = finesse,
+                                              focal_length = focal_length,
+                                              gap = gap,
                                               **constraints )
         self.log = print
         
@@ -46,26 +50,17 @@ class airy_model ( Parametric2DModel ):
         self.__rows = self.__discontinuum.shape [ 1 ]
         self.__cols = self.__discontinuum.shape [ 2 ]
         
-        self.__focal_length = 0.1
         self.__pixel_size = 9
         self.__reflectivity = 0.99
-
-        self.__fit = None
-
-        indices = numpy.indices ( ( self.__rows, self.__cols ) )
-        distances = numpy.sqrt ( ( indices [ 0 ] - self.__center [ 0 ] ) ** 2 +
-                                 ( indices [ 1 ] - self.__center [ 1 ] ) ** 2 )
-        grid = distances * self.__pixel_size * ( 1.e-6 )
-        self.__theta = numpy.arctan ( grid / self.__focal_length )
-        
+       
     @staticmethod
     def eval ( x,
                y,
                beam,
-               gap,
-               finesse ):
+               finesse,
+               focal_length,
+               gap ):
 
-        focal_length = 0.1
         pixel_size = 9
         reflectivity = 0.99
         wavelength = 6562.7797852
@@ -76,12 +71,13 @@ class airy_model ( Parametric2DModel ):
         center = ( 218, 253 )
 
         indices = numpy.indices ( ( rows, cols ) )
-        distances = numpy.sqrt ( ( indices [ 1 ] - center [ 0 ] ) ** 2 +
-                                 ( indices [ 0 ] - center [ 1 ] ) ** 2 )
+        distances = numpy.sqrt ( ( indices [ 0 ] - center [ 0 ] ) ** 2 +
+                                 ( indices [ 1 ] - center [ 1 ] ) ** 2 )
         grid = distances * pixel_size * ( 1.e-6 )
         theta = numpy.arctan ( grid / focal_length )
 
         result = numpy.zeros ( shape = ( rows, cols ) )
+
         # function of Airy I
         airy_function = 4.0 * ( ( finesse ** 2 ) ) / ( numpy.pi ** 2 ) 
         phase = 4.0 * numpy.pi * reflectivity * gap * numpy.cos ( theta ) / wavelength 
@@ -89,10 +85,6 @@ class airy_model ( Parametric2DModel ):
 
         return intensity
 
-#    @format_input
-#    def __call__ ( self, x ):
-#        return self.eval ( x, *self.param_sets )
-    
 def fit_Airy ( t_center = ( int, int ), 
                f_max_distance = None,
                f_min_distance = None,
@@ -107,14 +99,18 @@ def fit_Airy ( t_center = ( int, int ),
 #                                     log = log, 
                                      discontinuum = a_filtered )
     
+    airy_custom_model.focal_length = 0.1
+    airy_custom_model.focal_length.fixed = True
+    airy_custom_model.finesse = 15
+    airy_custom_model.finesse.fixed = True
 
     LevMarLSQFitter_fit = LevMarLSQFitter ( )
     
     y, x = numpy.mgrid [ : a_filtered.shape [ 1 ], : a_filtered.shape [ 2 ] ]
-    log ( "debug: x.shape = %s" % str ( x.shape ) )
-    log ( "debug: y.shape = %s" % str ( y.shape ) )
-    log ( "debug: x = %s" % str ( x ) )
-    log ( "debug: y = %s" % str ( y ) )
+    #log ( "debug: x.shape = %s" % str ( x.shape ) )
+    #log ( "debug: y.shape = %s" % str ( y.shape ) )
+    #log ( "debug: x = %s" % str ( x ) )
+    #log ( "debug: y = %s" % str ( y ) )
     
     result = numpy.zeros ( shape = a_filtered.shape )
         
@@ -123,12 +119,14 @@ def fit_Airy ( t_center = ( int, int ),
         #    warnings.simplefilter ( 'ignore' )
         data = a_filtered [ dep ]
         #log ( "debug: data.shape = %s" % str ( data.shape ) )
-        log ( "debug: data = %s" % str ( data ) )
+        #log ( "debug: data = %s" % str ( data ) )
+        airy_custom_model.gap = 1200 + dep
+        airy_custom_model.gap.fixed = True
         airy_model_fit = LevMarLSQFitter_fit ( airy_custom_model, x, y, data )
         #log ( "debug: airy_model_fit [ 'message' ] = %s" % str ( airy_model_fit [ 'message' ] ) )
         result [ dep ] = airy_model_fit ( x, y )
-        log ( "debug: result [ dep ] = %s" % str ( result [ dep ] ) )
-        log ( "debug: For plane %d, Airy fit parameters are: %s" % ( dep, str ( airy_model_fit.parameters ) ) )
+        #log ( "debug: result [ dep ] = %s" % str ( result [ dep ] ) )
+        log ( "debug: For plane %d, Airy fit parameters are: beam = %s, finesse = %s, focal_length = %s, gap = %s" % ( dep, str ( airy_model_fit.parameters [ 0 ] ), str ( airy_model_fit.parameters [ 1 ] ), str ( airy_model_fit.parameters [ 2 ] ), str ( airy_model_fit.parameters [ 3 ] ) ) )
 
     log ( "info: Airy fit took %ds." % ( time ( ) - start ) )
     return result
