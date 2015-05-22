0e923c9f4ef5764365620d1adec45d27e31db612
Renato Callado Borges <rborges@hush.com>
2015-05-22 15:40:09 +0200

    Removed gui and test code that is not used.

--- gui/gui.py
+++
@@ -1,7 +0,0 @@
-"""
-gui.py
-
-Provides access to Tuna's GUI namespace.
-"""
-
-from .window_2d_viewer import window_2d_viewer
--- gui/widget_tool.py
+++
@@ -1,6 +0,0 @@
-from PyQt4.QtGui import QPushButton, QWidget
-
-class tuna_tool ( QWidget ):
-    def __init__ ( self, name = str, *args, **kwargs ):
-        self.button = QPushButton ( text = name )
-        super ( tuna_tool, self).__init__ ( *args, **kwargs )
